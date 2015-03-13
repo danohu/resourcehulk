a30bab6809ac007c7de491a07943d7b9019eaba8
Dan O'Huiginn <daniel@ohuiginn.net>
2015-03-13 15:10:57 +0000

    convert to use the real db layout

--- hulk/admin.py
+++ hulk/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from hulk.models import Company, License, Document, Statement, Project
+from hulk.models import (
+    Company, Concession, Document, Statement, Project,
+    Commodity, ConcessionAlias, CompanyAlias)
 
-for table in (Company, License, Document,Statement,Project):
+for table in (Company, Concession, Document,Statement,Project,
+              Commodity, ConcessionAlias, CompanyAlias):
     admin.site.register(table)
 
 # Register your models here.
--- hulk/models.py
+++ hulk/models.py
@@ -1,6 +1,202 @@
 from django.db import models
 
 
+"""
+class BeneficialOwnership(models.Model):
+    owned_by = models.ForeignKey('Company')
+    owner_of = models.ForeignKey('Company')
+
+    class Meta:
+        managed = False
+        db_table = 'benneficial_ownership_table'
+
+
+class CommodityLink(models.Model):
+    commodity = models.ForeignKey('Commodity')
+    entity_id = models.CharField(max_length=200)
+
+    class Meta:
+        managed = False
+        db_table = 'commodity_link_table'
+
+class CompanyLink(models.Model):
+    statement = models.ForeignKey('Statement')
+    company = models.ForeignKey('Company')
+
+    class Meta:
+        managed = False
+        db_table = 'company_link_table'
+
+
+class ConcessionLink(models.Model):
+    statement = models.ForeignKey('Statement')
+    concession = models.ForeignKey('Concession')
+
+    class Meta:
+        managed = False
+        db_table = 'concession_link_table'
+
+class ProjectLink(models.Model):
+    statement = models.ForeignKey('Statement')
+    project = models.ForeignKey('Project')
+
+    class Meta:
+        managed = False
+        db_table = 'project_link_table'
+
+class ContractLink(models.Model):
+    statement = models.ForeignKey('Statement')
+    contract = models.ForeignKey('Contract')
+
+    class Meta:
+        managed = False
+        db_table = 'contract_link_table'
+
+
+"""
+
+class Commodity(models.Model):
+    commodity_id = models.CharField(primary_key=True, max_length=200)
+    commodity_name = models.CharField(max_length=200)
+
+    class Meta:
+        managed = False
+        db_table = 'commodity_table'
+
+    def __str__(self):
+        return self.commodity_name or '<untitled>'
+
+
+class CompanyAlias(models.Model):
+    company_alias = models.CharField(primary_key=True, max_length=200)
+    company_id = models.CharField(max_length=200)
+
+    class Meta:
+        managed = False
+        db_table = 'company_alias_table'
+
+    def __str__(self):
+        return self.company_alias or '<untitled>'
+
+
+
+class Company(models.Model):
+    company_id = models.CharField(primary_key=True, max_length=200)
+    open_lei_id = models.CharField(max_length=200, blank=True)
+    duns_number = models.CharField(max_length=200, blank=True)
+    company_name = models.CharField(max_length=200)
+    ticker_symbol = models.CharField(max_length=200, blank=True)
+    tax_id = models.CharField(max_length=200, blank=True)
+    open_corp_id = models.CharField(max_length=200, blank=True)
+    vat_id = models.CharField(max_length=200, blank=True)
+    company_url = models.CharField(max_length=200, blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'company_table'
+
+    def __str__(self):
+        return self.company_name or '<untitled>'
+
+
+class ConcessionAlias(models.Model):
+    concession_alias = models.CharField(primary_key=True, max_length=200)
+    concession_id = models.CharField(max_length=200)
+
+    class Meta:
+        managed = False
+        db_table = 'concession_alias_table'
+
+    def __str__(self):
+        return self.concession_alias or '<untitled>'
+
+
+class Concession(models.Model):
+    concession_id = models.CharField(primary_key=True, max_length=200)
+    concession_name = models.CharField(max_length=200)
+    unep_geo_id = models.CharField(max_length=200, blank=True)
+    country = models.CharField(max_length=200)
+
+    class Meta:
+        managed = False
+        db_table = 'concession_table'
+
+    def __str__(self):
+        return self.concession_name or '<untitled>'
+
+
+
+class Contract(models.Model):
+    contract_id = models.CharField(primary_key=True, max_length=200)
+    doc = models.ForeignKey('Document')
+    country = models.CharField(max_length=200)
+    sign_date = models.CharField(max_length=200)
+    title_type = models.CharField(max_length=200, blank=True)
+    source_url = models.CharField(max_length=200)
+    doc_cloud_id = models.CharField(max_length=200)
+    doc_cloud_url = models.CharField(max_length=200)
+    sign_year = models.CharField(max_length=200)
+
+    class Meta:
+        managed = False
+        db_table = 'contract_table'
+
+    def __str__(self):
+        return self.contract_id or '<untitled>'
+
+class DjangoMigrations(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class Document(models.Model):
+    doc_id = models.CharField(primary_key=True, max_length=200)
+    host_url = models.CharField(max_length=200)
+
+    class Meta:
+        managed = False
+        db_table = 'document_table'
+
+    def __str__(self):
+        return self.host_url or '<untitled>'
+
+
+
+class Project(models.Model):
+    project_id = models.CharField(primary_key=True, max_length=200)
+    country = models.CharField(max_length=200)
+
+    class Meta:
+        managed = False
+        db_table = 'project_table'
+
+
+class Statement(models.Model):
+    statement_id = models.CharField(primary_key=True, max_length=200)
+    doc = models.ForeignKey(Document)
+    statement_content = models.CharField(max_length=200)
+    definitive = models.BooleanField(default=False)
+
+    projects = models.ManyToManyField('Project', db_table='project_link_table')
+    companies = models.ManyToManyField('Company', db_table='company_link_table')
+    concessions = models.ManyToManyField('Concession', db_table='concession_link_table')
+    projects = models.ManyToManyField('Contract', db_table='contract_link_table')
+
+    class Meta:
+        managed = False
+        db_table = 'statement_table'
+
+    def __str__(self):
+        return self.statement_content or '<untitled>'
+
+"""
+
 class Company(models.Model):
     name = models.CharField(max_length=200)
 
@@ -12,6 +208,7 @@ class License(models.Model):
 
     def __str__(self):
         return self.name or '<untitled>'
+
 
 class Statement(models.Model):
     name = models.CharField(max_length=200)
@@ -52,3 +249,4 @@ class Document(models.Model):
         return self.title or '<untitled>'
 
 
+"""
