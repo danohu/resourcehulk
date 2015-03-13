20d38460dd2004cb72cad6298ef57e436b5b3246
Dan O'Huiginn <daniel@ohuiginn.net>
2015-03-13 09:54:14 +0000

    __str__ for models; README for db setup

--- README.txt
+++ README.txt
@@ -5,7 +5,11 @@ Planning/info at https://docs.google.com/document/d/1B9pZnaIlD9c3LUTO2B4oSJsT_pN
 # Server setup
 
 ```
-sudo apt-get install python3-pip virtualenvwrapper git
+sudo apt-get install python3-pip virtualenvwrapper git 
+sudo apt-get install postgresql postgresql-contrib
+sudo apt-get install libpq-dev python-dev 
+sudo apt-get install postgresql-server-dev-9.1
+
 bash ## start a new shell for virtualenvwrapper to start up
 mkvirtualenv --python=/usr/bin/python3 resourcehulk
 git clone https://github.com/danohuiginn/resourcehulk.git
@@ -13,3 +17,19 @@ cd resourcehulk
 pip install -r requirements.txt
 python manage.py runserver
 ```
+
+Database setup
+
+[For production, we're using connect.io. For dev, here's the local version]
+
+```
+sudo su - postgres
+createuser -P # & follow instructions to make resourcehulk user
+psql
+psql> create database resourcehulk
+psql> grant all privileges on database resourcehulk to resourcehulk
+```
+
+You may also need to modify postgres settings to not require peer authentication
+
+# Details of the serv
--- hulk/models.py
+++ hulk/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name or '<untitled>'
+
 class License(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name or '<untitled>'
 
 class Statement(models.Model):
     name = models.CharField(max_length=200)
@@ -23,15 +29,26 @@ class Statement(models.Model):
     projects = models.ManyToManyField('Project')
     companies = models.ManyToManyField('Company')
 
+    def __str__(self):
+        return self.name or '<untitled>'
+
+
 class Project(models.Model):
     name = models.CharField(max_length=200)
     country = models.CharField(max_length=50, blank=True) # should be choice
     # location -- should be some kind of geoid?
+
+    def __str__(self):
+        return self.name or '<untitled>'
 
 class Document(models.Model):
     title = models.TextField(blank=True)
     date = models.DateField(blank=True, null=True)
     source_url = models.URLField() # official source, eg. on EDGAR
     mirror_url = models.URLField() # our copy of it, eg on S3
+    
+
+    def __str__(self):
+        return self.title or '<untitled>'
 
 
