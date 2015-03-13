075269561eb5588be9d438fad459d2f69f9aa237
Dan O'Huiginn <daniel@ohuiginn.net>
2015-03-13 10:59:16 +0000

    very early work towards views

--- hulk/views.py
+++ hulk/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
+from hulk import models
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Not Implemented')
+
+def company(request, company_id):
+    company = get_object_or_404(models.Company, pk=company_id)
+    return HttpResponse('company %s ' % company.name)
+
+def project(request, project_id):
+    project = get_object_or_404(models.Project, pk=project_id)
+    return HttpResponse('project %s ' % project.name)
+
